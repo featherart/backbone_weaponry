ede47e303829fcbeef892b5f87f59915aead49d2
Feather <lisafeather@gmail.com>
2013-08-15 11:07:37 -0400

    added model and collection

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 // MODEL: weapon
+
+var Weapon = Backbone.Model.extend({
+	idAttributes: "slug",
+	defaults: {
+		name: "Aphrodite\'s Sword",
+		type: "Sword",
+		slug: "aphrodites-sword",
+		number_slain: "9001"
+	}
+});
+
 // collection: weapons
+
+var Weapons = Backbone.Collection.extend({
+	model: Weapon
+});
+
 // router: everything
 // views: AppView, ShowWeaponView, IndexWeaponsView
 // jQuery: Onload function
